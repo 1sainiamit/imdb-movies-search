c52f7b03a34a604ac0694d3a45f8df0f49f5a59e
Amit Saini <amitsaini2001.18@gmail.com>
2024-02-03 16:49:39 +0530

    Add initial project files and configurations

---
+++ src/MovieSearchForm.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import styles from "./MovieSearchForm.module.css";
+import axios from "axios";
+function MovieSearchForm({
+    addNewMovies,
+    setIsLoading,
+    setIsError,
+    setErrorMessage,
+}) {
+    const [movieName, setMovieName] = useState("");
+    async function fetchMovies() {
+        const URL = `https://www.omdbapi.com/?s=${movieName}&apikey=${
+            import.meta.env.VITE_API_KEY
+        }`;
+
+        setIsLoading(true);
+        setIsError(false);
+        try {
+            const response = await axios.get(URL);
+            // Response : true / false
+            if (response.data.Response === "False") {
+                throw new Error(response.data.Error);
+            }
+            addNewMovies(response.data.Search);
+        } catch (error) {
+            setIsError(true);
+
+            setErrorMessage(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetchMovies();
+    }
+    return (
+        <form className={`container ${styles.form}`} onSubmit={handleSubmit}>
+            <input
+                type="text"
+                onChange={(e) => setMovieName(e.target.value)}
+                value={movieName}
+                placeholder="Movie Name"
+            />
+            <button type="submit">Search</button>
+        </form>
+    );
+}
+
+export default MovieSearchForm;
